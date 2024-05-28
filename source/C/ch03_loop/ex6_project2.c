44464c100b7382a31d6ec15c10ee048060d564f0
comlec0703 <comlec0703@gmail.com>
2024-05-28 11:34:30 +0900

    variable

--- source/C/ch03_loop/ex6_project2.c
+++ source/C/ch03_loop/ex6_project2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void){
-    int floor ;
-    printf("몇단 피라미드를 쌓을까요(5~20)?");
-    scanf("%d", &floor);
+    int floor = 4 ;
+    // printf("몇단 피라미드를 쌓을까요(5~20)?");
+    // scanf("%d", &floor);
     for(int i=0 ; i<floor ; i++){
-        for(int j=i ; j<floor-1 ; j++){
+        for(int j=i ; j<floor ; j++){
             printf(" ");
         }
         for(int k=0 ; k<i*2+1 ; k++){
