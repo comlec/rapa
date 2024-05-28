477aee2a26284af3475b1dbbba0d371dc1db05db
comlec0703 <comlec0703@gmail.com>
2024-05-28 10:14:39 +0900

    variable

--- source/C/ch03_loop/ex2.3_비교_삼항연산자.c
+++ source/C/ch03_loop/ex2.3_비교_삼항연산자.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 // 비교연산자 : < <= > >= == , !=, ...
 // 삼항연산자 : (조건) ? (조건이 참일때의 값) : (조건이 거짓일때 값)
 int main(void){
     int n1 = 10, n2=5;
-    int result = n1<=n2;
-    printf("%d %s %d는 %d\n", n1, "<=", n2, result);
+    bool result = n1<=n2; // true나 false
+    printf("%d %s %d는 %s\n", n1, "<=", n2, result? "참":"거짓");
 }
