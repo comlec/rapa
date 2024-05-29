52399217d51d49dcfdb164d89bb3ade4e78caa73
comlec0703 <comlec0703@gmail.com>
2024-05-29 16:07:53 +0900

    variable

--- source/C/ch08_struct/ex2_3_구조체배열.c
+++ source/C/ch08_struct/ex2_3_구조체배열.c
@@ -4,6 +4,7 @@ struct student{
     char name[20]; // 이름 char* name;
     double grade;  // 학점
 };
+void stPrint(struct student s); // 함수선언
 int main(void){
     // 구조체 변수 선언과 초기화
     struct student sArr[3] = {  {101, "홍길동", 4.43},
@@ -11,6 +12,9 @@ int main(void){
                                 {103, "신길동", 4.01}};
     printf("학번\t이름\t학점\n");
     for(int idx = 0; idx<3 ; idx++){
-        printf("%d\t%s\t%.2lf\n", sArr[idx].id, sArr[idx].name, sArr[idx].grade);
-    }
+        stPrint(sArr[idx]); // 함수 호출
+    } // for
+} // main
+void stPrint(struct student s){
+    printf("%d\t%s\t%.2lf\n", s.id, s.name, s.grade);
 }
