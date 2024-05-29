073f7b22ef8b023a2ba1152617cce53591c054ec
comlec0703 <comlec0703@gmail.com>
2024-05-29 12:28:56 +0900

    variable

--- source/C/ch07_pointer/ex3_1_포인터로배열접근.c
+++ source/C/ch07_pointer/ex3_1_포인터로배열접근.c
@@ -8,4 +8,12 @@ int main(void){
     for(int idx=0 ; idx<3 ; idx++){
         printf("배열 ptr[%d] = %d\n", idx, ptr[idx]);
     }
+    ptr[0] = 99; ptr[1]=88; ptr[2]=77;
+    printf("== ptr 수정후 ===\n");
+    for(int idx=0 ; idx<3 ; idx++){
+        printf("배열 arr[%d] = %d\n", idx, *(arr+idx));
+    }
+    for(int idx=0 ; idx<3 ; idx++){
+        printf("배열 ptr[%d] = %d\n", idx, *(ptr+idx));
+    }
 }
