135c8b7e823fc8fe93848a3459dfe97037b9b2f6
comlec0703 <comlec0703@gmail.com>
2024-05-29 15:47:52 +0900

    variable

--- source/C/ch08_struct/ex2_2_구조체초기화.c
+++ source/C/ch08_struct/ex2_2_구조체초기화.c
@@ -5,10 +5,7 @@ struct student { // 구조체를 정의
     double grade; // 학점(student 구조체 멤버)
 };
 int main(void){
-    int i;
-    struct student s1;
-    i = 1;
-    s1.num = 101;
-    s1.grade = 4.43;
+    int i =1 ;
+    struct student s1 = {101, 4.43};
     printf("학번 = %d, 학점 = %.2lf\n", s1.num, s1.grade);
 }
