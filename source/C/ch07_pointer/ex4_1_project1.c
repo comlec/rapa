09ddd6b3031d35760bc1e3b5f1b9d97d16c8c879
comlec0703 <comlec0703@gmail.com>
2024-05-29 14:35:31 +0900

    variable

--- source/C/ch07_pointer/ex4_1_project1.c
+++ source/C/ch07_pointer/ex4_1_project1.c
@@ -7,4 +7,20 @@ int main(void){
     for(int idx=0 ; idx<6 ; idx++){
         printf("%d\t", lotto[idx]);
     }
+    sort(lotto, 6);
+    printf("\nÁ¤·ÄÈÄ : ");
+    for(int idx=0 ; idx<6 ; idx++){
+        printf("%d\t", lotto[idx]);
+    }
 }
+void sort(int* lotto, int cnt){
+    for(int i=0 ; i<cnt-1 ; i++){
+        for(int j=i+1 ; j<cnt ; j++){
+            if(lotto[i] > lotto[j]){
+                int temp = lotto[i];
+                lotto[i] = lotto[j];
+                lotto[j] = temp;
+            }//if
+        }//for - j
+    }//for - i
+} // sort
